57517b85121712493cd0b094feb6f8d0aefe9367
TLabAltoh <121733943+TLabAltoh@users.noreply.github.com>
2023-04-16 00:58:44 +0900

    マルチプレイ対応

---
+++ AFrame-test/main.js
@@ -0,0 +1,25 @@
+// const box = document.getElementById("my_box");
+
+// box.addEventListener("raycaster-intersected", (e)=>{
+//     box.setAttribute('color', "#F1C40F");
+// });
+
+// box.addEventListener("raycaster-intersected-cleared", (e)=>{
+//     box.setAttribute('color', "#D4AC0D");
+// });
+
+// AFRAME.registerComponent("vr-controller", {
+//     dependencies: ["raycaster"],// Important
+//     init: function(){
+//         console.log("vr-controller");
+//         this.el.addEventListener("triggerdown", function(e) {
+//             const text = document.getElementById("my_text");
+//             if(box.getAttribute("color") == "#F1C40F") {
+//                 if(text.getAttribute("value") == "AAAA")
+//                     text.setAttribute("value", "he elements of painting are the basic components or building blocks of a painting. In Western art, they are generally considered to be color, tone, line, shape, space, and texture. In general, we tend to agree that there are seven formal elements of art.");
+//                 else
+//                     text.setAttribute("value", "AAAA");
+//             }
+//         });
+//     }
+// });
